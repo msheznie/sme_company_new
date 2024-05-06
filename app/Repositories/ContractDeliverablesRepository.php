2c6bf709b89196873f518dc46f7292381713ddec
Ishfaq-A <61774663+Ishfaq-A@users.noreply.github.com>
2024-05-06 13:01:21 +0530

    chore(contract): contract milestone and deliverabled changes [CM-12] (#34)
    * feat(contract master): contract milestone and deliverables crud [CM-12]
    
    * chore(contract): contract milestone and deliverables changes [CM-12]
    
    * chore(contract): milestone and deliverables export excel [CM-12]
    
    ---------
    
    Co-authored-by: Ishfaq-A <ishfaq@osos.om>

--- app/Repositories/ContractDeliverablesRepository.php
+++ app/Repositories/ContractDeliverablesRepository.php
@@ -2,8 +2,15 @@
 
 namespace App\Repositories;
 
+use App\Helpers\General;
 use App\Models\ContractDeliverables;
+use App\Models\ContractMaster;
+use App\Models\ContractMilestone;
 use App\Repositories\BaseRepository;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Traits\CrudOperations;
 
 /**
  * Class ContractDeliverablesRepository
@@ -16,6 +23,8 @@ class ContractDeliverablesRepository extends BaseRepository
     /**
      * @var array
      */
+    use CrudOperations;
+
     protected $fieldSearchable = [
         'uuid',
         'contractID',
@@ -46,6 +55,10 @@ class ContractDeliverablesRepository extends BaseRepository
         return ContractDeliverables::class;
     }
 
+    protected function getModel(){
+        return new ContractDeliverables();
+    }
+
     public function getDeliverables($contractID, $companySystemID): array {
         $deliverables = ContractDeliverables::select('uuid', 'milestoneID', 'description', 'startDate', 'endDate')
             ->with([
@@ -67,5 +80,157 @@ class ContractDeliverablesRepository extends BaseRepository
             }
         }
         return $deliverablesArray;
+    }
+
+    public function createDeliverables(Request $request): array {
+        $companySystemID = $request->input('companySystemID');
+        $contractUuid = $request->input('contractUuid');
+        $description = $request->input('description');
+        $milestone = $request->input('milestone') ?? null;
+        $formattedStartDate = $request->input('formattedStartDate') ?? null;
+        $formattedEndDate = $request->input('formattedEndDate') ?? null;
+        $uuid = bin2hex(random_bytes(16));
+        $milestoneID = 0;
+
+        if(ContractDeliverables::where('uuid', $uuid)->exists()) {
+            return [
+                'status' => false,
+                'message' => trans('common.deliverable_uuid_already_exists')
+            ];
+        }
+
+        $contractMaster = ContractMaster::select('id')->where('uuid', $contractUuid)
+            ->where('companySystemID', $companySystemID)
+            ->first();
+        if(empty($contractMaster)) {
+            return [
+                'status' => false,
+                'message' => trans('common.contract_id_not_found')
+            ];
+        }
+        if($milestone != null) {
+            $milestoneExists = ContractMilestone::select('id')
+                ->where('uuid', $milestone)
+                ->where('companySystemID',$companySystemID)
+                ->first();
+
+            $milestoneID = $milestoneExists['id'] ?? 0;
+        }
+        $contractId = $contractMaster['id'];
+        try{
+            DB::beginTransaction();
+            $insertDeliverables = [
+                'uuid' => $uuid,
+                'contractID' => $contractId,
+                'description' => $description,
+                'milestoneID' => $milestoneID,
+                'startDate' => $formattedStartDate ? Carbon::parse($formattedStartDate) : null,
+                'endDate' => $formattedEndDate ? Carbon::parse($formattedEndDate) : null,
+                'companySystemID' => $companySystemID,
+                'created_by' => General::currentEmployeeId(),
+                'created_at' => Carbon::now()
+            ];
+            ContractDeliverables::insert($insertDeliverables);
+
+            DB::commit();
+            return ['status' => true, 'message' => trans('common.deliverable_created_successfully')];
+        } catch (\Exception $ex){
+            DB::rollBack();
+            return ['status' => false, 'message' => $ex->getMessage(), 'line' => __LINE__];
+        }
+    }
+    public function updateDeliverable(Request $request, $id): array {
+        $companySystemID = $request->input('companySystemID');
+        $contractUuid = $request->input('contractUuid');
+        $description = $request->input('description');
+        $milestone = $request->input('milestone') ?? null;
+        $formattedStartDate = $request->input('formattedStartDate') ?? null;
+        $formattedEndDate = $request->input('formattedEndDate') ?? null;
+        $milestoneID = 0;
+
+        $contractMaster = ContractMaster::select('id')->where('uuid', $contractUuid)
+            ->where('companySystemID', $companySystemID)
+            ->first();
+
+        if(empty($contractMaster)) {
+            return [
+                'status' => false,
+                'message' => trans('common.contract_id_not_found')
+            ];
+        }
+        if($milestone != null) {
+            $milestoneExists = ContractMilestone::select('id')
+                ->where('uuid', $milestone)
+                ->where('companySystemID',$companySystemID)
+                ->first();
+
+            $milestoneID = $milestoneExists['id'] ?? 0;
+        }
+        try{
+            DB::beginTransaction();
+            $insertDeliverables = [
+                'description' => $description,
+                'milestoneID' => $milestoneID,
+                'startDate' => $formattedStartDate ? Carbon::parse($formattedStartDate) : null,
+                'endDate' => $formattedEndDate ? Carbon::parse($formattedEndDate) : null,
+                'updated_by' => General::currentEmployeeId(),
+                'updated_at' => Carbon::now()
+            ];
+            ContractDeliverables::where('id', $id)->update($insertDeliverables);
+
+            DB::commit();
+            return ['status' => true, 'message' => trans('common.deliverable_updated_successfully')];
+        } catch (\Exception $ex){
+            DB::rollBack();
+            return ['status' => false, 'message' => $ex->getMessage(), 'line' => __LINE__];
+        }
+    }
+
+    public function getDeliverablesExcelData(Request $request): array {
+        $contractUuid = $request->input('contractUUid') ?? null;
+        $companySystemID = $request->input('companySystemID') ?? 0;
+        $startDateText = 'Start Date';
+        $endDateText = 'End Date';
+
+        $contractMaster = ContractMaster::select('id')
+            ->where('uuid', $contractUuid)
+            ->where('companySystemID', $companySystemID)
+            ->first();
+        if(empty($contractMaster)) {
+            return [
+                'status' => false,
+                'message' => trans('common.contract_id_not_found')
+            ];
+        }
+        $contractID = $contractMaster['id'];
+        $deliverables = ContractDeliverables::select('milestoneID', 'description', 'startDate', 'endDate')
+            ->with([
+                'milestone' => function ($q) {
+                    $q->select('id', 'title');
+                }
+            ])
+            ->where('contractID', $contractID)
+            ->where('companySystemID', $companySystemID)
+            ->get();
+
+        $data[0]['Description'] = "Description";
+        $data[0]['Milestone'] = "Milestone";
+        $data[0][$startDateText] = $startDateText;
+        $data[0][$endDateText] = $endDateText;
+        if($deliverables) {
+            foreach ($deliverables as $key => $deliverable){
+                $data[$key+1]['Description'] = $deliverable['description'];
+                $data[$key+1]['Milestone'] = $deliverable['milestone']['title'] ?? '-';
+                $data[$key+1][$startDateText] = $deliverable['startDate'] ?
+                    Carbon::parse($deliverable['startDate'])->format('Y-m-d') : '-';
+                $data[$key+1][$endDateText] = $deliverable['endDate'] ?
+                    Carbon::parse($deliverable['endDate'])->format('Y-m-d') : '-';
+            }
+        }
+        return [
+            'status' => true,
+            'message' => trans('common.successfully_data_loaded'),
+            'deliverable' => $data
+        ];
     }
 }
