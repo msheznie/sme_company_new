956e0401d34cc98edf7a0fed9017025b4328602e
AflalAbdeen <44675108+AflalAbdeen@users.noreply.github.com>
2024-07-25 15:44:17 +0530

    feat(Contract): API Development to show the contract ID / Reference in Finance & Proc [CM-284] (#196)
    * feat(Contract): API Development to show the contract ID / Reference in Finance & Proc [CM-284]
    
    * fix(Contract): API Development to show the contract ID / Reference in Finance & Proc [CM-284]
    
    * fix(Contract): API Development to show the contract ID / Reference in Finance & Proc [CM-284]

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -463,3 +463,15 @@
         <span>Contract Milestone Penalty Masters</span>
     </a>
 </li>
+<li class="nav-item {{ Request::is('thirdPartySystems*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('thirdPartySystems.index') }}">
+        <i class="nav-icon icon-cursor"></i>
+        <span>Third Party Systems</span>
+    </a>
+</li>
+<li class="nav-item {{ Request::is('thirdPartyIntegrationKeys*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('thirdPartyIntegrationKeys.index') }}">
+        <i class="nav-icon icon-cursor"></i>
+        <span>Third Party Integration Keys</span>
+    </a>
+</li>
